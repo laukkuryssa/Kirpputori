782a7d8ae493009af667e40d869a277cf34c7599
0141456267 <timo.hautamaki@hotmail.com>
2016-05-13 20:20:10 +0300

    ei etene

---
+++ db/migrate/20160513161700_add_user_id_to_id.rb
@@ -0,0 +1,4 @@
+class AddUserIdToId < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512190115) do
+ActiveRecord::Schema.define(version: 20160513161700) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "title"
